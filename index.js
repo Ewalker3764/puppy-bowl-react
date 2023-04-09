3bc6b774f1961819685cd334d97b2685d4333b95
Ewalker3764 <127571863+Ewalker3764@users.noreply.github.com>
2023-04-09 15:31:48 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-
-const BasicComponentNameHere = () => { return ( <div> <p>Hello World!</p> </div> ) };
-
-const appElement = document.getElementById("app");
-const root = createRoot(appElement);
-root.render(<BasicComponentNameHere />);
